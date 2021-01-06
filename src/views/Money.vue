61de7042a014c7024f5617b0b316f17ff5eac362
torreyxian <xianxt0570@163.com>
2021-01-06 23:18:29 +0800

    money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>Money.vue
+    <div class="nav-wrapper">
+        <div class="content">
+            <p>Money.vue</p>
+        </div>
         <Nav/>
     </div>
 </template>
@@ -11,5 +14,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        border: 5px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        border: 2px solid blue;
+        overflow: auto;
+        flex-grow: 1;
+    }
 </style>
