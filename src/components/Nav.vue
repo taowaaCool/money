6e722f408c882c34b6d81d45ee6e1af337349b71
torreyxian <xianxt0570@163.com>
2021-01-08 00:20:04 +0800

    添加路由激活

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
     <nav>
-        <router-link to="/money" class="item">
+        <router-link to="/money" class="item" active-class="selected">
             <Icon name="money"/>
             记账
         </router-link>
-        <router-link to="/labels" class="item">
+        <router-link to="/labels" class="item" active-class="selected">
             <Icon name="label"/>
             标签
         </router-link>
-        <router-link to="/statistics" class="item">
+        <router-link to="/statistics" class="item" active-class="selected">
             <Icon name="statistic"/>
             统计
         </router-link>
@@ -27,22 +27,23 @@
 <style lang='scss' scoped>
     nav {
         display: flex;
-        box-shadow: 0 0 3px rgb(0, 0, 0, 0.25);
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
         flex-direction: row;
-
-        .item {
+        font-size: 12px;
+        > .item {
             padding: 2px 0;
-            width: 33.33%;
+            width: 33.33333%;
             display: flex;
             justify-content: center;
             align-items: center;
             flex-direction: column;
-            font-size: 12px;
-
             .icon {
                 width: 32px;
                 height: 32px;
             }
+        }
+        > .item.selected{
+            color: red;
         }
     }
 </style>
