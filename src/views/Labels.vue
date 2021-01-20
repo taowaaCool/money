4ece4e35c41201f6e9524b5bbed7edc4994ea85d
torreyxian <xianxt0570@163.com>
2021-01-20 14:27:13 +0800

    创建标签页

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,8 +18,8 @@
                 <Icon name="right"></Icon>
             </li>
         </ol>
-        <div>
-            <button>新建标签</button>
+        <div class="createTag-wrapper">
+            <button class="createTag">新建标签</button>
         </div>
     </layout>
 </template>
@@ -34,5 +34,37 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .tags{
+        background: white;
+        padding-left: 16px;
+        > li {
+            font-size: 16px;
+            min-height: 44px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border: 1px solid #e6e6e6;
+            border-left: hidden;
+            border-top: hidden;
+            svg {
+                height: 20px;
+                width: 20px;
+                margin-right: 16px;
+                color: #333;
+            }
+        }
+    }
+    .createTag{
+        background-color: #767676;
+        color: white;
+        border-radius: 4px;
+        border: none;
+        height: 40px;
+        padding: 0 16px;
+        &-wrapper {
+            text-align: center;
+            padding: 16px;
+            margin-top: 44-16px;
+        }
+    }
 </style>
