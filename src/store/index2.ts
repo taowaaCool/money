3077e856ecdec2a4278c1029d99fb7829f7a9fee
torreyxian <xianxt0570@163.com>
2021-02-01 17:07:36 +0800

    模块化的 store

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,30 +1,12 @@
-import {recordListModel} from '@/model/recordListModel';
-import {tagListModel} from '@/model/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
   // record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
+  ...recordStore,
 
-// tag store
-  tagList: tagListModel.fetch(),
-  createTag: (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('标签名重复了');
-    } else if (message === 'success') {
-      window.alert('添加成功');
-    }
-  },
-  findTag: (id: string) => {
-    return window.tagList.filter(t => t.id === id)[0];
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
-  updateTag: (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
+  // tag store
+  ...tagStore,
 };
 
 export default store;
