eacdb384d068c0ac37b7a2b3b4e4dac0c96ab412
torreyxian <xianxt0570@163.com>
2021-02-15 21:43:42 +0800

    Statistics.vue 中的 RecordList[] 无法找到

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,10 +3,30 @@
         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
         <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
         <div>
-            type:{{type}}
-            <br>
-            interval: {{interval}}
+            <ol>
+                <li v-for="(group,index) in result" :key="index">
+                    <h3>{{group.title}}</h3>
+                    <ol>
+                        <li v-for="item in group.items" :key="item.id">
+                            <span>￥{{item.amount}}</span>
+                            <span>{{item.createdAt}}</span>
+                        </li>
+                    </ol>
+                </li>
+            </ol>
         </div>
+        <!--        <ol>-->
+        <!--            <li v-for="(group,index) in result" :key="index">-->
+        <!--                <h3 class="title">{{group.title}}</h3>-->
+        <!--                <ol>-->
+        <!--                    <li v-for="item in group.items" :key="item.id" class="record">-->
+        <!--                        <span>{{tagString(item.tags)}}</span>-->
+        <!--                        <span class="notes">{{item.notes}}</span>-->
+        <!--                        <span>￥{{item.amount}}</span>-->
+        <!--                    </li>-->
+        <!--                </ol>-->
+        <!--            </li>-->
+        <!--        </ol>-->
     </layout>
 </template>
 
@@ -22,13 +42,36 @@
             }
         }
     }
-    ::v-deep .interval-tabs-item{
+
+    ::v-deep .interval-tabs-item {
         height: 48px;
+    }
+
+    %item {
+        padding: 8px 16px;
+        line-height: 24px;
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+    }
+
+    .title {
+        @extend %item
+    }
+
+    .record {
+        background: white;
+        @extend %item;
+    }
+
+    .notes {
+        margin-right: auto;
+        margin-left: 16px;
+        color: #999;
     }
 </style>
 
 <script lang='ts'>
-  import Types from '@/components/Money/Types.vue';
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import Tabs from '@/components/Tabs.vue';
@@ -36,9 +79,36 @@
   import recordTypeList from '@/constants/recordTypeList';
 
   @Component({
-    components: {Tabs, Types},
+    components: {Tabs},
   })
   export default class Statistics extends Vue {
+
+    tagString(tags: Tag[]) {
+      return tags.length === 0 ? '无' : tags.join(',');
+    }
+
+    get recordList() {
+      return (this.$store.state as RootState).recordList;
+    }
+
+    get result() {
+      const {recordList} = this;
+      type Items = RecordList[];
+      type HashTableValue = {title:string, items: RecordList[]};
+
+      const hashTable: { [key: string]: HashTableValue } = {};
+      for (let i = 0; i < recordList.length; i++) {
+        const [date, time] = recordList[i].createdAt!.split('T');
+        hashTable[date] = hashTable[date] || [];
+        hashTable[date].items.push(recordList[i]);
+      }
+      return hashTable;
+    }
+
+    beforeCreate() {
+      this.$store.commit('fetchRecords');
+    }
+
     type = '+';
     interval = 'day';
     recordTypeList = recordTypeList;
